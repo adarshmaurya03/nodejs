699e202d0e794742e03655c3b1663c72ece0096c
Adarsh Maurya <adarshm7053@gmail.com>
2024-05-15 01:22:23 +0530

    this is miscell file

---
+++ figlate/miscell/index.js
@@ -0,0 +1,12 @@
+const express =require("express");
+const app=express();
+const port=8080;
+app.get("/register",(req,res)=>{
+    res.send("standard GET response");
+})
+app.post("/register",(req,res)=>{
+    res.send("standard post response");
+})
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
+})
